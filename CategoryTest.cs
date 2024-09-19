9feb91657cba75faac5096305778853ffb1c5479
Erik Tonin <144070405+Erik-Tonin@users.noreply.github.com>
2024-09-18 21:18:09 -0300

    first commit

---
+++ CategoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+public class CategoryTest
+{
+	public CategoryTest()
+	{
+		[Fact()]
+		public void Instantiate()
+		//Arrange
+		var validData = new 
+		{
+			Name = "category name",
+			Description = "category description"
+		}
+		//Act
+		var category = new Category(validData.Name, validData.Description);
+		//Assert
+		Assert.NotNull(category);
+		Assert.Equal(validData.Name, validData.Description);
+		Assert.Equal(validData.Description, validData.Description);
+    }
+}
