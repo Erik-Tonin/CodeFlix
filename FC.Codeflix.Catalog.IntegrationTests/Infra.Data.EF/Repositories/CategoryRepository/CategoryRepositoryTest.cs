ebe19e4ac4dc44001d3fc90c5d067a65c712bc83
Erik Tonin <EATTonin@apoiocdhu.sp.gov.br>
2024-11-01 14:44:55 -0300

    Create infra

---
+++ FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using FC.CodeFlix.Catalog.Infra.Data.Ef;
+using FluentAssertions;
+using Repository = FC.CodeFlix.Catalog.Infra.Data.Ef.Repositories;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository
+{
+    [Collection(nameof(CategoryRepositoryTestFixture))]
+    public class CategoryRepositoryTest
+    {
+        private readonly CategoryRepositoryTestFixture _fixture;
+
+        public CategoryRepositoryTest(CategoryRepositoryTestFixture fixture)
+            => _fixture = fixture;
+
+        [Fact]
+        [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+        public async Task Insert()
+        {
+            CodeFlixCatalogDbContext dbContext = _fixture.CreateDbContext();
+            var exampleCategory = _fixture.GetExampleCategory();
+            var categoryRepository = new Repository.CategoryRepository(dbContext);
+
+            await categoryRepository.Insert(exampleCategory, CancellationToken.None);
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+
+            var dbCategory = await dbContext.Categories.FindAsync(exampleCategory.Id);
+            dbCategory.Should().NotBeNull();
+            dbCategory!.Name.Should().Be(exampleCategory.Name);
+            dbCategory.Description.Should().Be(exampleCategory.Description);
+            dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+            dbCategory.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+        }
+    }
+}
